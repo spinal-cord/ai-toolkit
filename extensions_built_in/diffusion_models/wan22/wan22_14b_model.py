bc4fc4ba94799be414a18c852a5f9bdd1553fa7f
spinal-cord <78312519+spinal-cord@users.noreply.github.com>
2026-03-04 00:00:00 +0000

    2

--- extensions_built_in/diffusion_models/wan22/wan22_14b_model.py
+++ extensions_built_in/diffusion_models/wan22/wan22_14b_model.py
@@ -417,7 +417,7 @@ class Wan2214bModel(Wan21):
         super().__init__(
             device=device,
             model_config=model_config,
-            dtype=torch.float8_e4m3fn,
+            dtype=dtype,
             custom_pipeline=custom_pipeline,
             noise_scheduler=noise_scheduler,
             **kwargs,
@@ -688,9 +688,8 @@ class Wan2214bModel(Wan21):
         
         # Skip quantization if dtype is already float8 (pre-quantized model)
         is_already_quantized = dtype in (torch.float8_e4m3fn, torch.float8_e5m2)
-        
-        print("dtype == ",dtype)
-        print("torch.float8_e4m3fn == ",torch.float8_e4m3fn)
+        self.print_and_status_update(str("dtype == ",dtype))
+        self.print_and_status_update(str("torch.float8_e4m3fn == ",torch.float8_e4m3fn)
         
         if self.model_config.quantize and not is_already_quantized and self.model_config.accuracy_recovery_adapter is None:
             self.print_and_status_update("Quantizing HIGH noise Transformer")
